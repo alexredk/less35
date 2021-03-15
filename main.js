da464db48199e76793869e8994c032b1c722f73c
Student7 <Student7@student.com>
2021-03-15 17:57:55 +0200

    a

---
+++ main.js
@@ -0,0 +1,24 @@
+// arr=['Roma' , 'Katya' ,' Max', ' Yulya ' , ' poluchili ' ,' podarok']
+// arr[2]=' Max '
+// alert[arr[0]+arr[1]+arr[4]+arr[5]]
+// obj={'Kolya ':'1000', 'Roma':'2000','Yulya':'5000' }
+// alert(obj['Roma'])
+// arr={
+//     'Yulya' :[' 3000' , '32000' , ' 40000'],
+//     ' Roma ' :[' 8 ' , ' 9 ',' 10']
+// }
+// alert (arr[ 'Yulya'] [0])
+
+    // arr= {
+    //     ' Girls ': ['Katya' , ' Yulya ', ' Sonya ' , ' Vika '],
+    //     ' Boys ' : [' Max ' , ' Roma ' , ' Ilya ' , ' Artem ' , ' Tima ' , ' Denis ']
+    // }
+    // alert(arr[' Girls '] [0]+ arr[ ' Boys '] [5] )
+   
+ arr=[2, 3, 4 , 5]
+ let a = 1
+ for(i = 0 ; i < 4 ;i++ ){
+     a=arr[i]*a
+     alert(a)
+ }
+ 
